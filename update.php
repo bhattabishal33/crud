a0bbdcf9f22dbde61565f6a496534e1c7956cdb0
bishal <bhattabishal330@gmail.com>
2022-10-16 19:42:13 +0545

    updates

--- update.php
+++ update.php
@@ -1,14 +1,16 @@
 <?php
     include 'conn.php';
     $id = $_POST['id'];
-?>
+    echo '
     <form action="update2.php" method="post">
-    <label for="col2">Enter data in first collum</label>
-    <input type="text" name="col2" id="col2">
-    <label for="col3">Enter data in first collum</label>
-    <input type="text" name="col3" id="col3">
-    <label for="col4">Enter data in first collum</label>
-    <input type="number" name="col4" id="col4">
-    <input type="hidden" name="id" value="1">
-    <input type="submit" value="submit">
-</form>
+        <label for="col2">Enter data in first collum</label>
+        <input type="text" name="col2" id="col2">
+        <label for="col3">Enter data in first collum</label>
+        <input type="text" name="col3" id="col3">
+        <label for="col4">Enter data in first collum</label>
+        <input type="number" name="col4" id="col4">
+        <input type="hidden" name="id" value="'.$id.'">
+        <input type="submit" value="submit">
+    </form>';
+?>
+    
--- update2.php
+++ update2.php
@@ -1,9 +1,13 @@
 <?php
   include 'conn.php';
-  echo $_POST['id'];
-  //$cmd = 'UPDATE `tablename` SET `col2`="hello",`col3`="hi",`col4`= 12 WHERE `col1` = '.$_POST['id'].';';
-  /*if(mysqli_query($conn,$cmd))
+  $id = $_POST['id'];
+  $col2 = $_POST['col2'];
+  $col3 = $_POST['col3'];
+  $col4 = $_POST['col4'];
+  echo $id.$col2.$col3.$col4;
+  $cmd = 'UPDATE `tablename` SET `col2`="'.$col2.'",`col3`="'.$col3.'",`col4`= '.$col4.' WHERE `col1` = '.$id.';';
+  if(mysqli_query($conn,$cmd))
   {
-    header('location:display.php');
-  } */
+    echo "<script>window.location.href= 'display.php';</script>";
+  } 
 ?>
